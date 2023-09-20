6ef6b6b9313a381aa66151b64751d60c7e2a8a4f
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-09-20 10:01:05 -0300

    [feat] header initiated

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("templates/header.php");
+?>
+
+<main class="container">
+  <h1>Veja os jogos cadastrados</h1>
+</main>
+
+<?php
+require_once("templates/footer.php");
+?>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,13 @@
+<?php
+require_once("config/url.php");
+
+$user = "";
+
+if (isset($_SESSION['user'])) {
+  $user = $_SESSION['user'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -6,7 +16,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="styles/global.css">
   <title>Games Review</title>
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="<?= $BASE_URL ?>">
+        Games Review
+      </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="">Incluir jogo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Meus jogos</a>
+          </li>
+          <li class="nav-item">
+            <?php if ($user): ?>
+              <form action="" method="POST">
+                <button class="btn btn-danger" type="submit">Sair
+                  </a>
+              </form>
+            <?php else: ?>
+              <a class="btn btn-primary" href="">Login</a>
+            <?php endif; ?>
+          </li>
+        </ul>
+      </div>
+  </nav>
